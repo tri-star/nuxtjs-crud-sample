2e088e7b80b98826031a96b53d45c7419a86a13a
tristar <tristar.2nd@gmail.com>
2023-03-13 23:36:54 +0900

    FigmaのスタイルをTailwindのテーマに反映

---
+++ tailwind.config.js
@@ -0,0 +1,78 @@
+module.exports = {
+  content: [
+    './pages/**/*.vue',
+    './components/**/*.vue'
+  ],
+  theme: {
+    'colors': {
+      'sidemenu': {
+        'bg': '#232323',
+        'on': '#dbdbdb'
+      },
+      'primary': {
+        'main': '#5162ff',
+        'on': '#ecf0ff',
+        'main-01': '#6776ff',
+        'disabled': '#878ec9',
+        'shade03': '#0011a8'
+      },
+      'header': {
+        'main': '#a7b0ff',
+        'on': '#ffffff'
+      },
+      'main': {
+        'bg': '#f0f0f0',
+        'text': '#000000',
+        'section-bg': '#ffffff',
+        'section-title': '#404040',
+        'content-bg': '#ffffff',
+        'content-border': '#6f6f6f',
+        'link': '#0029ff',
+        'alert': '#ff0000'
+      },
+      'secondary': {
+        'main03': '#42ff00'
+      },
+      'form': {
+        'label': '#565656',
+        'row-border': '#000000',
+        'memo-text': '#000000',
+        'error': '#ff0000'
+      },
+      'plain': {
+        'main': '#f8f8f8',
+        'on': '#1f1f1f',
+        'main-01': '#ffffff',
+        'disabled': '#bababa',
+        'shade02': '#d9d9d9'
+      },
+      'table': {
+        'border': '#d1d1d1'
+      },
+      'transparent': {
+        'hover': '#000000',
+        'on': '#1f1f1f',
+        'alert': '#ff0000',
+        'alert01': '#ff0000'
+      }
+    },
+    'fontSize': {
+      'sm': '0.75rem',
+      'base': '1rem',
+      'lg': '1.25rem'
+    },
+    'fontFamily': {
+      'roboto': 'Roboto'
+    },
+    'boxShadow': {
+      'main/section-shadow': '0px 4px 10px -2px rgba(0,0,0,0.25)'
+    },
+    'borderRadius': {
+      'none': '0',
+      'xs': '0.25rem',
+      'sm': '0.3125rem',
+      'default': '0.5rem',
+      'lg': '5.375rem'
+    }
+  }
+}
