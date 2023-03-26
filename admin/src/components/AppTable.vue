1b334036b08be87c4f7e1e9d30a5c24c0acc28ce
tristar <tristar.2nd@gmail.com>
2023-03-27 00:03:22 +0900

    テーブルの外観調整

--- admin/src/components/AppTable.vue
+++ admin/src/components/AppTable.vue
@@ -10,6 +10,14 @@
   padding: 10px;
 }
 
+:deep(th):nth-child(1) {
+  @apply rounded-tl-sm;
+}
+
+:deep(th):nth-last-child(1) {
+  @apply rounded-tr-sm;
+}
+
 :deep(td) {
   @apply border-b border-table-border bg-main-content-bg;
   padding: 5px;
