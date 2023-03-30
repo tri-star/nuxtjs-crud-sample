24e30376e4b3584e4c8eb79c5abb99eb36c60791
tristar <tristar.2nd@gmail.com>
2023-03-31 01:13:28 +0900

    ヘッダメニューのホバー時のエフェクトを調整

--- admin/src/components/admin/layout/header/HeaderIcon.vue
+++ admin/src/components/admin/layout/header/HeaderIcon.vue
@@ -27,10 +27,37 @@ div {
   height: 32px;
   border-radius: 50%;
   transition: background-color 0.5s;
+  position: relative;
 }
 
 .link:hover {
-  background-color: rgba(255, 255, 255, 0.2);
+  /* background-color: rgba(255, 255, 255, 0.2); */
+}
+
+.link:hover::before {
+  content: '';
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 0;
+  height: 0;
+  border-radius: 50%;
+  background-color: rgba(255, 255, 255, 0.1);
+  animation: hover-effect 0.4s ease forwards;
+  backdrop-filter: blur(10px);
+}
+
+@keyframes hover-effect {
+  from {
+    width: 0;
+    height: 0;
+  }
+  to {
+    width: 40px;
+    height: 40px;
+    opacity: 1;
+  }
 }
 
 span {
